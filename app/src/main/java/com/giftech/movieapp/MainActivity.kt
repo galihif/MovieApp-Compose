d7db177e797a7b17ecde6965c52adc4616dea148
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-09-23 20:13:25 +0700

    ui: create LazyColumn with MovieRow item

--- app/src/main/java/com/giftech/movieapp/MainActivity.kt
+++ app/src/main/java/com/giftech/movieapp/MainActivity.kt
@@ -3,9 +3,19 @@ package com.giftech.movieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giftech.movieapp.ui.theme.MovieAppTheme
@@ -40,11 +50,57 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MainContent() {
-    Surface(
-        color = MaterialTheme.colors.background
+fun MainContent(
+    movieList: List<String> = listOf(
+        "1",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "3",
+        "4",
+    )
+) {
+    Column(
+        Modifier.padding(12.dp)
     ) {
-        Text("Hello")
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(120.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = 8.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image"
+                )
+            }
+            Text(movie)
+        }
     }
 }
 
