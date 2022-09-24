07338271abad4cfe6547ccbbb456728c1ebfc331
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-09-24 10:39:14 +0700

    ui: create HomeScreen.kt

---
+++ app/src/main/java/com/giftech/movieapp/screens/home/HomeScreen.kt
@@ -0,0 +1,102 @@
+package com.giftech.movieapp.screens.home
+
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun HomeScreen(
+    navController: NavController
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = Color.Blue,
+                elevation = 4.dp
+            ) {
+                Text("Movies")
+            }
+        }
+    ) {
+        MainContent(navController)
+    }
+}
+
+@Composable
+fun MainContent(
+    navController: NavController,
+    movieList: List<String> = listOf(
+        "1",
+        "2",
+        "2",
+        "2",
+        "2",
+        "2",
+        "3",
+        "4",
+    )
+) {
+
+    val mContext = LocalContext.current
+
+    Column(
+        Modifier.padding(12.dp)
+    ) {
+        LazyColumn {
+            items(items = movieList) { movie ->
+                MovieRow(movie = movie){
+                    Toast.makeText(mContext, it, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String, onItemClick: (String) -> Unit) {
+
+    Card(
+        Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(120.dp)
+            .clickable {
+                onItemClick("Click $movie")
+            },
+        shape = RoundedCornerShape(16.dp),
+        elevation = 8.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                elevation = 4.dp
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image"
+                )
+            }
+            Text(movie)
+        }
+    }
+}
