d4c4c8979132b96b49ad3ffbee269796dbf671e6
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-09-24 11:36:36 +0700

    ui: restructuring layout

--- app/src/main/java/com/giftech/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/giftech/movieapp/screens/details/DetailsScreen.kt
@@ -1,21 +1,33 @@
 package com.giftech.movieapp.screens.details
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun DetailsScreen(
     navController: NavController,
-    movie:String?
+    movie: String?
 ) {
-    Scaffold {
-        MainContent(movie)
+    Surface(
+        Modifier.fillMaxSize()
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "$movie",
+                style = MaterialTheme.typography.h5
+            )
+        }
     }
-}
-
-@Composable
-fun MainContent(movie: String?) {
-    Text("$movie")
 }
