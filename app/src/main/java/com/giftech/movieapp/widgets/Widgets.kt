ba0fb5272ddddada86681ca8858e25bd006054eb
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-09-26 12:09:05 +0700

    ui: show image from url with async image

--- app/src/main/java/com/giftech/movieapp/widgets/Widgets.kt
+++ app/src/main/java/com/giftech/movieapp/widgets/Widgets.kt
@@ -3,15 +3,17 @@ package com.giftech.movieapp.widgets
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.giftech.movieapp.model.Movie
 import com.giftech.movieapp.model.getMovies
 
@@ -45,10 +47,15 @@ fun MovieRow(
                 shape = RectangleShape,
                 elevation = 4.dp
             ) {
-                Icon(
-                    imageVector = Icons.Default.AccountBox,
-                    contentDescription = "Movie Image"
+
+                AsyncImage(
+                    model = movie.images[0],
+                    contentDescription = null
                 )
+//                Icon(
+//                    imageVector = Icons.Default.AccountBox,
+//                    contentDescription = "Movie Image"
+//                )
             }
             Column(
                 Modifier.padding(4.dp)
