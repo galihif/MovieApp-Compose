cc696f752ec5430014f921c0547e3e2e47e4e21f
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-09-26 11:38:33 +0700

    ui: add director and release year

--- app/src/main/java/com/giftech/movieapp/widgets/Widgets.kt
+++ app/src/main/java/com/giftech/movieapp/widgets/Widgets.kt
@@ -3,22 +3,25 @@ package com.giftech.movieapp.widgets
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giftech.movieapp.model.Movie
+import com.giftech.movieapp.model.getMovies
 
 
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClick: (String) -> Unit) {
+fun MovieRow(
+    movie: Movie = getMovies()[0],
+    onItemClick: (String) -> Unit = {}
+) {
 
     Card(
         Modifier
@@ -47,7 +50,22 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit) {
                     contentDescription = "Movie Image"
                 )
             }
-            Text(movie.title)
+            Column(
+                Modifier.padding(4.dp)
+            ) {
+                Text(
+                    movie.title,
+                    style = MaterialTheme.typography.h6
+                )
+                Text(
+                    "Director ${movie.director}",
+                    style = MaterialTheme.typography.caption
+                )
+                Text(
+                    "Released: ${movie.year}",
+                    style = MaterialTheme.typography.caption
+                )
+            }
         }
     }
 }
