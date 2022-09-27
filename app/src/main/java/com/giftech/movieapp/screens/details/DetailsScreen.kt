4819401859458841aeff15a8d7d7e033a2aa2c72
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-09-27 20:39:43 +0700

    show movierow and movie images in DetailsScreen.kt

--- app/src/main/java/com/giftech/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/giftech/movieapp/screens/details/DetailsScreen.kt
@@ -2,21 +2,27 @@ package com.giftech.movieapp.screens.details
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.giftech.movieapp.model.Movie
+import com.giftech.movieapp.model.getMovies
+import com.giftech.movieapp.widgets.MovieRow
 
 @Composable
 fun DetailsScreen(
     navController: NavController,
     movieId: String?
 ) {
+    val newMovieList = getMovies().filter { it.id == movieId }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -41,15 +47,37 @@ fun DetailsScreen(
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
-                Text(
-                    "$movieId",
-                    style = MaterialTheme.typography.h5
+                MovieRow(
+                    movie = newMovieList.first()
                 )
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text("Movie Images")
+                HorizontalScrollableImageView(newMovieList)
             }
         }
     }
 
 
+}
+
+@Composable
+private fun HorizontalScrollableImageView(newMovieList: List<Movie>) {
+    LazyRow {
+        items(newMovieList.first().images) { image ->
+            Card(
+                Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = 4.dp
+            ) {
+                AsyncImage(
+                    model = image,
+                    contentDescription = "",
+                )
+            }
+        }
+    }
 }
